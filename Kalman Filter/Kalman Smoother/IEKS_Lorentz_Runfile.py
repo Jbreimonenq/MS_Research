6fd10b9baa14c6716fadccc84a9efc272a573348
Jbreimonenq <josh.reimonenq@gmail.com>
2022-11-17 10:53:30 -0500

    Kalman Smoother
    Added Extended Kalman Smoother

---
+++ Kalman Filter/Kalman Smoother/IEKS_Lorentz_Runfile.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 16 23:23:04 2022
+
+@author: reimoj
+"""
+import numpy as np
+import matplotlib.pyplot as plt 
+from InexactLineSearch_IEKS import GN_IEKS_ILS
+from lorentz import lorentz
+from EKF_Plotter import plotIEKS as plot
+
+#Start of Main Code -----------------------------------------------------------
+#Defining Variables
+dt = 0.01
+sim_t = 3
+sigma = 13
+row = 10
+beta = 8/3
+
+H = np.array([[1, 0, 0]])
+in_states = len(H)
+Q = 1e-6 * np.eye(3) 
+R = 1e-0* np.eye(in_states)
+P = 1e0*np.eye(3)
+mean = np.zeros(in_states)
+x0 = np.array([1,1,1]).reshape((3, 1))
+
+states = 3
+
+
+#Run Code
+#x = env.nextstate(x0)
+#j = env.Jacobian(x0)
+#print(j)
+env = lorentz(sigma, row, beta, H, dt)
+
+
+
+# Create measurement
+T = int(np.round((sim_t+dt)/dt))
+s = np.array([1,1,1])
+y_list = []
+GT_state = []
+for i in range(T):
+    y = env.measurement(s) + np.random.multivariate_normal(mean, R)
+    y_list.append(y)
+    GT_state.append(s)
+    s = env.nextstate(s)
+
+kf = GN_IEKS_ILS(y_list, env, x0, P, Q, R)
+xp, xs = kf.run_R_EIKS()
+
+#xp = np.stack(xp, axis=0)
+#xs = np.stack(xs, axis=0)
+#a = np.stack(a, axis=0)
+#print(xs)
+
+plot(dt = dt, m_value = y_list, p_value = xs, a_value = GT_state, state = 0)
+#plot(dt = dt, p_value = p, a_value = a, state = 1) 
+#plot(dt = dt, p_value = p, a_value = a, state = 2)
